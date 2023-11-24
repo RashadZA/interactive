0a669318cf052b4181906529f4809f431c264929
RashadZA <97411218+RashadZA@users.noreply.github.com>
2023-11-24 20:06:02 +0600

    Connected project to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:interactive/core/routes/app_pages.dart';
+import 'package:interactive/core/server/firebase_options.dart';
 import 'package:interactive/core/utils/design_utils.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
